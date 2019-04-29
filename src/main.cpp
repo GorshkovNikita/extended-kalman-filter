7961d753e563868b18518d2e0a47850d0f7b321b
Nikita Gorshkov <gorshkov.nikita.alexandrovich@gmail.com>
2019-04-29 08:25:17 +0300

    Fixed Eigen library installation

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,12 @@
 #include <iostream>
+#include "Eigen/Dense"
+
+using namespace std;
+using namespace Eigen;
 
 int main() {
+    VectorXd vec(2);
+    vec << 1, 2;
+    cout << vec << endl;
     return 0;
 }
