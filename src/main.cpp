9158dc4144277bfb2d6f1569f5ad2e770dd312ae
Nikita Gorshkov <gorshkov.nikita.alexandrovich@gmail.com>
2019-04-29 08:35:43 +0300

    Added json library as submodule

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,18 @@
 #include <iostream>
 #include "Eigen/Dense"
+#include "json.hpp"
 
 using namespace std;
 using namespace Eigen;
+using namespace nlohmann;
 
 int main() {
     VectorXd vec(2);
     vec << 1, 2;
     cout << vec << endl;
+    json j = {
+        {"a", 1}
+    };
+    cout << j.dump() << endl;
     return 0;
 }
