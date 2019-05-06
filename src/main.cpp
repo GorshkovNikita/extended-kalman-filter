b7b6ca10a9c1feeae3aebaecc0162c721f327c77
Nikita Gorshkov <gorshkov.nikita.alexandrovich@gmail.com>
2019-05-06 20:27:56 +0300

    Added uWebSockets library as submodule

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,27 @@
 #include <iostream>
 #include "Eigen/Dense"
 #include "json.hpp"
+#include "App.h"
 
 using namespace std;
 using namespace Eigen;
 using namespace nlohmann;
 
 int main() {
-    VectorXd vec(2);
-    vec << 1, 2;
-    cout << vec << endl;
-    json j = {
-        {"a", 1}
-    };
-    cout << j.dump() << endl;
+//    VectorXd vec(2);
+//    vec << 1, 2;
+//    cout << vec << endl;
+//    json j = {
+//        {"a", 1}
+//    };
+//    cout << j.dump() << endl;
+    int port = 4567;
+    uWS::App().get("hello", [](auto *res, auto *req) {
+        res->writeHeader("Content-Type", "text/html; charset=utf-8")->end("Hello HTTP!");
+    }).listen(port, [port](auto *token) {
+        if (token) {
+            std::cout << "Listening on port " << port << std::endl;
+        }
+    }).run();
     return 0;
 }
